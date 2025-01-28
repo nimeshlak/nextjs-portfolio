d5c135350efcf6a96184c520ae8047ecfc502b36
Nimesh Lakshitha <nimeshlakshitha2001@gmail.com>
2025-01-29 00:13:49 +0530

    Updated home page alignments

--- src/app/Components/BlogCard.tsx
+++ src/app/Components/BlogCard.tsx
@@ -1,13 +1,12 @@
 import Link from 'next/link';
 
 interface CardProps {
-  image: string; // Path to the preview image
   title: string; // Title of the website
   description: string; // Short description
   link: string; // Link to the website
 }
 
-const Card: React.FC<CardProps> = ({ image, title, description, link }) => {
+const Card: React.FC<CardProps> = ({ title, description, link }) => {
   return (
     <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 w-full sm:w-[350px] md:w-[400px] lg:w-[450px]">
       <div className="p-4">
